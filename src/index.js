bf7e2d066303a27d5a854129e8238c6e34a9571c
HiImZsofi <zsofia.gyori00@gmail.com>
2022-10-19 14:42:56 +0200

    console log

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
     return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
 };
-var _Football_goalHome, _Football_goalAway;
+var _Football_goalHome, _Football_goalAway, _Calvinball_winner;
 class Football {
     constructor(winner, date, goalHome, goalAway) {
         _Football_goalHome.set(this, void 0);
@@ -23,5 +23,54 @@ class Football {
     result() {
         return "Football match: " + __classPrivateFieldGet(this, _Football_goalHome, "f") + "-" + __classPrivateFieldGet(this, _Football_goalAway, "f");
     }
+    kiir() {
+        console.log(this.result());
+    }
 }
 _Football_goalHome = new WeakMap(), _Football_goalAway = new WeakMap();
+class Marathon {
+    constructor(winner, date, runtime) {
+        this.winner = winner;
+        this.date = date;
+        this.runtime = runtime;
+    }
+    result() {
+        var minute = Math.floor(this.runtime / 60);
+        var seconds = this.runtime % minute;
+        return "Marathon: " + minute + "m " + seconds + "s";
+    }
+    kiir() {
+        console.log(this.result());
+    }
+}
+class Calvinball {
+    constructor(winner, date) {
+        _Calvinball_winner.set(this, void 0);
+        __classPrivateFieldSet(this, _Calvinball_winner, winner, "f");
+        this.date = date;
+    }
+    set winner(winner) {
+        if (winner == "Calvin" || winner == "Hobbes") {
+            this.winner = winner;
+        }
+    }
+    result() {
+        var rnd = Math.random() * 100 + 1;
+        return "Calvinball: " + rnd + " points";
+    }
+    kiir() {
+        console.log(this.result());
+    }
+}
+_Calvinball_winner = new WeakMap();
+let sports = [
+    new Football("barca", new Date("2019-01-16"), 1, 2),
+    new Football("atleti", new Date("2020-01-16"), 1, 5),
+    new Marathon("random1", new Date("2010-12-18"), 130),
+    new Marathon("random2", new Date("2010-02-18"), 152),
+    new Calvinball("random3", new Date("2017-04-15")),
+    new Calvinball("random4", new Date("2013-02-18")),
+];
+for (let sport of sports) {
+    sport.kiir();
+}
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ interface Results{
     
 
     result() : string;
+    kiir() : void;
 }
 
 class Football implements Results{
@@ -22,6 +23,72 @@ class Football implements Results{
     result(): string {
         return "Football match: " + this.#goalHome + "-" + this.#goalAway;
     }
+
+    kiir(): void {
+        console.log(this.result());
+    }
     
 
+}
+
+class Marathon implements Results{
+    winner: string;
+    date: Date;
+    runtime : number;
+
+    constructor(winner: string, date: Date, runtime: number){
+        this.winner = winner;
+        this.date = date;
+        this.runtime = runtime;
+    }
+
+
+    result(): string {
+        var minute = Math.floor(this.runtime / 60);
+        var seconds = this.runtime % minute;
+        return "Marathon: " + minute + "m " + seconds + "s";    
+    }
+
+    kiir(): void {
+        console.log(this.result());
+    }
+    
+}
+
+class Calvinball implements Results{
+    #winner: string;
+    date: Date;
+
+    constructor(winner: string, date: Date){
+        this.#winner = winner;
+        this.date = date;
+    }
+
+    set winner(winner: string){
+        if(winner == "Calvin" || winner == "Hobbes"){
+            this.winner = winner;
+        }
+    }
+    result(): string {
+        var rnd = Math.random()*100 + 1;
+        return "Calvinball: " + rnd + " points";
+    }
+
+    kiir(): void {
+        console.log(this.result());
+    }
+    
+}
+
+let sports : Results[] = [
+    new Football("barca", new Date("2019-01-16"), 1, 2),
+    new Football("atleti", new Date("2020-01-16"), 1, 5),
+    new Marathon("random1", new Date("2010-12-18"), 130),
+    new Marathon("random2", new Date("2010-02-18"), 152),
+    new Calvinball("random3", new Date("2017-04-15")),
+    new Calvinball("random4", new Date("2013-02-18")),
+];
+
+for(let sport of sports){
+    sport.kiir();
 }
